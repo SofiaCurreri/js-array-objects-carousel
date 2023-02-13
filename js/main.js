4d76cf0962e68b6a6bdb800288e34bc767c9266d
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-13 15:43:44 +0100

    struttura slider + thumbs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // Dato un array di oggetti letterali con: url dell'immagine, titolo, descrizione
 // Creare un carosello come nella foto allegata.
 
-
-
 // Milestone 2:
 // Aggiungere il ** ciclo infinito ** del carosello.Ovvero se la miniatura attiva è la prima
 // e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima
@@ -18,8 +16,6 @@
 // BONUS 3:
 // Aggiungere bottoni di start / stop e di inversione del meccanismo di autoplay.
 
-
-
 // Milestone 0:
 // Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico:
 // costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
@@ -29,68 +25,62 @@
 // dinamicamente il carosello. Al click dell'utente sulle frecce verso sinistra o destra,
 // l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
 
-const images = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    },
+const slides = [
+  {
+    image: "img/01.webp",
+    title: "Marvel's Spiderman Miles Morale",
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+  },
 
-    {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    },
+  {
+    image: "img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+  },
 
-    {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    },
+  {
+    image: "img/03.webp",
+    title: "Fortnite",
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  },
 
-    {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    },
+  {
+    image: "img/04.webp",
+    title: "Stray",
+    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+  },
 
-    {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
+  {
+    image: "img/05.webp",
+    title: "Marvel's Avengers",
+    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+  },
 ];
 
-const slidesContainer = document.querySelector(".items");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+//* RECUPERARE ELEMENTI HTML
+const slidesContainer = document.getElementById("main-container");
+const thumbsContainer = document.getElementById("thumbs-container");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const stopAutoplayButton = document.getElementById("stop-autoplay");
+const invertiAutoplayButton = document.getElementById("inverti-autoplay");
+
+//* CONTATORE IMMAGINE ACTIVE
 let activeImg = 0;
-for (let i = 0; i < images.length; i++) {
-    const currentImg = images[i].image;
-    let classesImg = "item ";
 
-    if (i == activeImg) {
-        classesImg += " active ";
-    }
+//* GENERARE SLIDES E THUMBS
+slides.forEach((slide, index) => {
+  slidesContainer.innerHTML += ` 
+    <div class = "slide ${index == activeImg ? " active" : ""}">
+        <img src = "./${slide.image}"> 
+        <div class = "slide-text">
+            <h2> ${slide.title} </h2>
+            <p> ${slide.text} </p>
+        </div>
+    </div>`;
 
-    const slide = `
-        <div class = "${classesImg}">
-            <img src = "${currentImg}" alt="">
-        </div>`;
-
-    slidesContainer.innerHTML += slide;
-}
-
-prevButton.addEventListener(
-    "click",
-    function () {
-
-    }
-);
-
-nextButton.addEventListener(
-    "click",
-    function () {
-
-    }
-);
+  thumbsContainer.innerHTML += `
+    <div class = "thumb" data-index = "${index}">
+        <img src = "./${slide.image}">
+    </div>`;
+});
